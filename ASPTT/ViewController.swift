7f18480f523db9531dc98563fda3627df87fb22c
Sullivan VITIELLO <vitiel_s@g03-r6p05.local>
2016-04-12 17:34:10 +0200

    Initial Commit

---
+++ ASPTT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASPTT
+//
+//  Created by Sullivan VITIELLO on 12/04/16.
+//  Copyright © 2016 Sullivan VITIELLO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
