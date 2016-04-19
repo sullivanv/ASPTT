f71c862cd407b8995e8f8e50aa1b8fc913388c3a
Sullivan VITIELLO <vitiel_s@g03-r6p05.local>
2016-04-19 17:40:13 +0200

    ajout de la reservation de creneau et envoi des reponses client a l'api

--- ASPTT/Question_sport.swift
+++ ASPTT/Question_sport.swift
@@ -10,10 +10,32 @@ import Foundation
 
 class Question_sport: UIViewController {
     
+    
+    @IBOutlet weak var qu1: UITextField!
+    @IBOutlet weak var qu2: UITextField!
+    @IBOutlet weak var qu3: UITextField!
+    @IBOutlet weak var qu4: UITextField!
+    @IBOutlet weak var qu5: UITextField!
+    
     @IBAction func send_sport(sender: AnyObject) {
+    
         let prefs = NSUserDefaults.standardUserDefaults()
         let client = prefs.objectForKey("clientencours") as! String
         
+        let q1:NSString = qu1.text!
+        let q2:NSString = qu2.text!
+        let q3:NSString = qu3.text!
+        let q4:NSString = qu4.text!
+        let q5:NSString = qu5.text!
+        
+        var json = "{\"email\": \"" + client
+        json = json + "\", \"1\": \"" + (q1 as String)
+        json = json + "\", \"2\": \"" + (q2 as String)
+        json = json + "\", \"3\": \"" + (q3 as String)
+        json = json + "\", \"4\": \"" + (q4 as String)
+        json = json + "\", \"5\": \"" + (q5 as String) + "\"}"
+        
+        print(json)
     }
     func hideKeyboardWhenTappedAround() {
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
